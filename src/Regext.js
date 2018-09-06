e797a3b22aa72aed43fc74952d4e1f93c6cb5c94
Dennis Weiershäuser <dennis.weiershaeuser@informatik.hs-fulda.de>
2018-09-06 14:42:47 +0200

    pausing on regex regex

--- src/Regext.js
+++ src/Regext.js
@@ -5,23 +5,36 @@
     ghi/gm  # comment three
 
     USAGE:
-    const foo1 = new Regext(/something/gm)
+    // you need to double \ when creating a regex by string, for the first \ to remain, when using string literals
+    const foo = new Regext(`
+        something\\n     # a comment
+        anotherthing    \\# no comment spaces and # should remain
+    `, "gm")
+
+    RULES:
+    - always escape spaces or tabs with \ in your regex
+
+    QUESTIONS:
+    - whats with # that shouldnt be treated as comment?
+    - how to treat `/something/` or `/something/gm`?
+
+    STILL UNDER CONSTRUCTION:
+    regex:  [\t\ ]*(?<!\\)\#.*$|(?<=\\)\#|\s*
+    test: 
+            something\n     # a comment
+        anotherthing    \# no comment spaces and hashtag should remain
+    regex: [\t\ ]*(?<!\\)\#.*
+    test:  \# no comment followed by # a comment
     
-    const foo2 = new Regext("something", "gm")
-    foo2.add("anotherthing") => result: /somethinganotherthing/gm
-
-    const foo3 = new Regext(
-        /
-            something\n
-            anotherthing
-        /gm
-    )
-
 */
 // export default class Regext {
 class Regext {
-    // constructor(regex: String | RegExp){
-    constructor(regex){
-
+    // constructor(regex: String, flags: String){
+    constructor(regex, flags){
+        regex = regex.replace(/[\t\ ]*(?<!\\)#.*$|\s*/gm, "")
+        console.log("regex after regexing reged:", regex)
+        regex = new RegExp(`${regex}`, `${flags}`)
+        console.log("result:", regex)
+        return regex 
     }
 }
